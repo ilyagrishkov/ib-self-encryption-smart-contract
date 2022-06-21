36650f40e0f7d609b906750e3630d55f834d5ac6
Ilya Grishkov <ilyagrishkov@icloud.com>
2022-06-21 12:35:08 +0300

    Fix naming

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hyperledger/fabric-samples/asset-transfer-basic/chaincode-go
+module ibse
 
 go 1.14
 
---
+++ ibse.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"ibse/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting chaincode: %v", err)
+	}
+}
